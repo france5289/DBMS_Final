703eb490806d77cee329c8dbf19dd20120e108f6
france5289 <st980407@gmail.com>
2020-06-14 21:23:03 +0800

    finish movi_manager_GUI and fix SQL commit bug

--- home.py
+++ home.py
@@ -37,6 +37,8 @@ def home():
     except IndexError as e:
         print(e)
         print('No data in Movie table')
+    mysql.connection.commit()
+    cur.close()
     return render_template('home.html', movie_list1=movie_dict1, movie_list2=movie_dict2, movie_list3=movie_dict3)
 
 @app.route('/data_manager', methods=['GET'])
@@ -57,6 +59,8 @@ def sql_playground():
     movies = []
     for item in result:
         movies.append(list(item))
+    mysql.connection.commit()
+    cur.close()
     return render_template('sql_playground.html', data=movies)
 
 @app.route('/sql_query', methods=['POST'])
@@ -68,6 +72,8 @@ def sql_query():
     query = request.form['SQL']
     print(query)
     cur.execute(f"{query}")
+    mysql.connection.commit()
+    cur.close()
     return sql_playground()
 
 @app.route('/movie_manager_GUI', methods=['GET','POST'])
@@ -81,6 +87,8 @@ def movie_manager_GUI():
     movies = []
     for item in result:
         movies.append(list(item))
+    mysql.connection.commit()
+    cur.close()
     return render_template('movie_manager_GUI.html', data=movies)
 
 @app.route('/insert_movie', methods=['GET','POST'])
@@ -91,16 +99,94 @@ def insert_movie():
     cur = mysql.connection.cursor()
     try:
         pkey = request.form['Movie_Name']
-        var1 = request.form['Rating']
-        var2 = request.form['Tomato_score']
-        var3 = request.form['IMDB_score']
-        var4 = request.form['Wolrd_Box']
-        var5 = request.form['movie_image']
-        cur.execute(f"INSERT INTO Movie VALUES ('{pkey}', '{var1}', '{var2}', '{var3}', '{var4}', '{var5}')")
+        if request.form['Rating'] == '':
+            var1 = 'NULL'
+        else:
+            var1 = request.form['Rating']
+
+        if request.form['Tomato_score'] == '':
+            var2 = 'NULL'
+        else:
+            var2 = request.form['Tomato_score']
+        
+        if request.form['IMDB_score'] == '':
+            var3 = 'NULL'
+        else:
+            var3 = request.form['IMDB_score']
+        
+        if request.form['World_Box'] == '':
+            var4 = 'NULL'
+        else:
+            var4 = request.form['World_Box']
+        
+        if request.form['movie_image'] == '':
+            var5 = 'NULL'
+        else:
+            var5 = request.form['movie_image']
+        
+        cur.execute(f"INSERT INTO Movie VALUES ('{pkey}', '{var1}', {var2}, {var3}, {var4}, '{var5}')")
+        mysql.connection.commit()
+        cur.close()
         return movie_manager_GUI()
     except Exception as e:
         print(e)
         return render_template('not_found.html')
+
+@app.route('/update_movie', methods=['POST'])
+def update_movie():
+    cur = mysql.connection.cursor()
+    try:
+        pkey = request.form['Movie_Name']
+        if request.form['Rating'] == '':
+            cur.execute(f"SELECT Rating FROM Movie WHERE Movie.Movie_Name='{pkey}'")
+            var1 = cur.fetchone()[0]
+        else:
+            var1 = request.form['Rating']
+        
+        if request.form['Tomato_score'] == '':
+            cur.execute(f"SELECT Tomato_score FROM Movie WHERE Movie.Movie_Name='{pkey}'")
+            var2 = cur.fetchone()[0]
+        else:
+            var2 = request.form['Tomato_score']
+        
+        if request.form['IMDB_score'] == '':
+            cur.execute(f"SELECT IMDB_score FROM Movie WHERE Movie.Movie_Name='{pkey}'")
+            var3 = cur.fetchone()[0]
+        else:
+            var3 = request.form['IMDB_score']
+        
+        if request.form['World_Box'] == '':
+            cur.execute(f"SELECT World_Box FROM Movie WHERE Movie.Movie_Name='{pkey}'")
+            var4 = cur.fetchone()[0]
+        else:
+            var4 = request.form['World_Box']
+        
+        if request.form['movie_image'] == '':
+            cur.execute(f"SELECT movie_image FROM Movie WHERE Movie.Movie_Name='{pkey}'")
+            var5 = cur.fetchone()[0]
+        else:
+            var5 = request.form['movie_image']
+        cur.execute(f"UPDATE Movie SET Rating='{var1}', Tomato_score={var2}, IMDB_score={var3}, World_Box = {var4}, movie_image = '{var5}' WHERE Movie.Movie_Name = '{pkey}'")
+        mysql.connection.commit()
+        cur.close()
+        return movie_manager_GUI()
+    
+    except Exception as e:
+        print(e)
+        return render_template('not_found.html')
+
+
+@app.route('/delete_movie', methods=['POST'])
+def delete_movie():
+    '''
+    given movie name then delete it
+    '''
+    cur = mysql.connection.cursor()
+    pkey = request.form['Movie_Name']
+    cur.execute(f"DELETE FROM Movie WHERE Movie.Movie_Name = '{pkey}'")
+    mysql.connection.commit()
+    cur.close()
+    return movie_manager_GUI()
 
 @app.route('/Searcher', methods=['POST'])
 def searcher():
@@ -116,6 +202,7 @@ def searcher():
         if search_type == 'Movie_Name':
             cur.execute(f"SELECT * FROM Movie WHERE Movie_Name='{key}'")
             result = cur.fetchone()
+            print(result)
             movie_info = {'MName':result[0], 'Rating':result[1], 'Tomato':result[2],
                         'IMDB':result[3], 'BOX':result[4], 'img':result[-1]}
             cur.execute(f"SELECT count(Dislike.user_id) FROM Movie, Dislike WHERE Movie.Movie_name = Dislike.Mname and Movie.Movie_name='{key}'GROUP BY Movie.Movie_name")
@@ -137,8 +224,14 @@ def searcher():
                 plats.append(item[0])
             cur.execute(f"SELECT Produced.ProdName, Produced.DNAME FROM Movie, Produced WHERE Movie.Movie_name = Produced.MNAME and Movie.Movie_name='{key}'")
             prod = cur.fetchall()
-            producer = prod[0][0]
-            director = prod[0][1]
+            if len(prod) != 0:
+                producer = prod[0][0]
+                director = prod[0][1]
+            else:
+                producer = 'N/A'
+                director = 'N/A'
+            mysql.connection.commit()
+            cur.close()
             return render_template('movie_result.html', result=movie_info, ndlike=dislike_count, lcount=love_count, plats=plats, producer=producer, director=director)
         elif search_type == 'UID':
             # get user basic info from User table
@@ -158,7 +251,8 @@ def searcher():
             dislike_movie = []
             for item in result:
                 dislike_movie.append(item[0])
-            
+            mysql.connection.commit()
+            cur.close()
             return render_template('user_result.html', user=user_info, fav=fav_movie, dis=dislike_movie)
         else:
             raise ValueError('Invalid Search Type! (From searcher())')
@@ -178,21 +272,29 @@ def filter():
             cur.execute("SELECT Movie_Name, IMDB_score, Tomato_score, Movie_image FROM Movie WHERE IMDB_score = ( SELECT MAX(IMDB_score) From Movie )")
             result = cur.fetchone()
             movie_info = {'MName':result[0], 'IMDB':result[1], 'Tomato':result[2], 'Img':result[3]}
+            mysql.connection.commit()
+            cur.close()
             return render_template('filter_result.html', movie=movie_info)
         elif filter_type == 'First_Tomato':
             cur.execute("SELECT Movie_Name, IMDB_score, Tomato_score, Movie_image FROM Movie WHERE Tomato_score = ( SELECT MAX(Tomato_score) From Movie )")
             result = cur.fetchone()
             movie_info = {'MName':result[0], 'IMDB':result[1], 'Tomato':result[2], 'Img':result[3]}
+            mysql.connection.commit()
+            cur.close()
             return render_template('filter_result.html', movie=movie_info)
         elif filter_type == 'Low_IMDB':
             cur.execute("SELECT Movie_Name, IMDB_score, Tomato_score, Movie_image FROM Movie WHERE IMDB_score = ( SELECT MIN(IMDB_score) From Movie )")
             result = cur.fetchone()
             movie_info = {'MName':result[0], 'IMDB':result[1], 'Tomato':result[2], 'Img':result[3]}
+            mysql.connection.commit()
+            cur.close()
             return render_template('filter_result.html', movie=movie_info)
         elif filter_type == 'Low_Tomato':
             cur.execute("SELECT Movie_Name, IMDB_score, Tomato_score, Movie_image FROM Movie WHERE Tomato_score = ( SELECT MIN(Tomato_score) From Movie )")
             result = cur.fetchone()
             movie_info = {'MName':result[0], 'IMDB':result[1], 'Tomato':result[2], 'Img':result[3]}
+            mysql.connection.commit()
+            cur.close()
             return render_template('filter_result.html', movie=movie_info)
         elif filter_type == 'Most_Rated':
             cur.execute("SELECT Movie_Name, Movie_image, count(Love.user_id) FROM Movie, Love WHERE Movie.Movie_name = Love.Mname GROUP BY Movie.Movie_name HAVING count(Love.user_id) > 2;")
@@ -200,6 +302,8 @@ def filter():
             movie_list = []
             for item in result:
                 movie_list.append({'Mname':item[0], 'img':item[1], 'count':item[2]})
+            mysql.connection.commit()
+            cur.close()
             return render_template('filter_result_user_rated.html', movies=movie_list)    
     except Exception as e:
         print(e)
